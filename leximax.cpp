d3a249cfbec2fb3d9985fa6a39c15e506367d2aa
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2020-07-21 13:18:38 +0100

    updates

--- leximax.cpp
+++ leximax.cpp
@@ -12,9 +12,27 @@
 #include <old_packup/cl_types.hh>
 #include <old_packup/ReadCNF.hh>
 
-typedef std::vector<std::forward_list<std::pair<unsigned long, unsigned long>>> SNET;
+typedef std::vector<std::forward_list<std::pair<LINT, LINT>>> SNET;
 
-void encode_network(std::forward_list<std::forward_list<long>*> &hard_clauses, std::vector<unsigned long> &elems_to_sort, unsigned long *id_count, std::forward_list<unsigned long> *objective, SNET &sorting_network)
+void encode_fresh(ReadCNF &hard, BasicClause *cl, LINT fresh_var)
+{
+    // fresh_var implies cl
+    vector<LINT> *lits;
+    lits->push_back(-fresh_var);
+    for(Literator it = cl->begin(); it != cl->end(); ++it){
+        lits->push_back(*it);
+    };
+    hard.get_clause_vector().push_back(hard.get_clauses().create_clause(lits));
+    // not fresh_var implies not cl
+    for(Literator it = cl->begin(); it != cl->end(); ++it){
+        lits.clear();
+        lits.push_back(-*it);
+        lits.push_back(fresh_var);
+        hard.get_clause_vector().push_back(hard.get_clauses().create_clause(lits));s
+    };    
+}
+
+void encode_network(std::forward_list<std::forward_list<long>*> &hard_clauses, std::vector<LINT> &elems_to_sort, LINT *id_count, std::forward_list<LINT> *objective, SNET &sorting_network)
 {    
     if(elems_to_sort.size() == 2){
         // single comparator.
@@ -31,24 +49,39 @@ int main(int argc, char *argv[])
     for(short i{2}; i < argc; ++i){
         read_objectives.push_back(ReadCNF(argv[i]));
     };
-    std::vector<std::forward_list<unsigned long>*> objectives(num_objectives);
-    unsigned long id_count{ hard.get_max_id() };
-    std::vector<std::pair<unsigned long, unsigned long>> sorted_vecs(num_objectives);
-    // convert objective function clauses to a sum of variables
+    std::vector<std::forward_list<LINT>*> objectives(num_objectives);
+    LINT id_count{ hard.get_max_id() };
+    std::vector<std::pair<LINT, LINT>> sorted_vecs(num_objectives);
+    // convert objective function clauses to a sum of variables.
     for(short i{0}; i < num_objectives; ++i){
-        
+        ReadCNF *obj = read_objectives[i];
+        vector<BasicClause*> cls = obj->get_clause_vector();
+        std::forward_list<LINT> *obj_conv;
+        for(LINT j{0}; j < cls.size(); ++j){
+            BasicClause *cl = cls[j];
+            if(cl->size() > 1 || cl->begin() < 0){
+                LINT fresh_var = id_count + 1;
+                id_count++;
+                // encode fresh_var
+                encode_fresh(hard, cl, fresh_var);
+                obj_conv->push_front(fresh_var);
+            }
+            else
+                obj_conv->push_front(cl->begin());
+        }
+        objectives[i] = obj_conv;        
     }
 
     // encode with odd even merge sorting network
     for(short i{0}; i < num_objectives; ++i){
         ReadCNF *objective = objectives[i];
         
-        unsigned long num_terms = objective->get_clause_vector().size();
-        std::pair<unsigned long, unsigned long> sorted_vec (id_count + 1, id_count + num_terms);
+        LINT num_terms = objective->get_clause_vector().size();
+        std::pair<LINT, LINT> sorted_vec (id_count + 1, id_count + num_terms);
         sorted_vecs[i]=sorted_vec;
         id_count += num_terms;
         SNET sorting_network(num_terms);
-        std::vector<unsigned long> elems_to_sort(objective->begin(),objective->end());
+        std::vector<LINT> elems_to_sort(objective->begin(),objective->end());
         encode_network(hard_clauses, elems_to_sort, &id_count, objective, sorting_network);
         ++it1;
         ++it2;
