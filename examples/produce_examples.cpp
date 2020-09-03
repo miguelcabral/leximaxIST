7000b97d45e5384464dfa737f660a52cd021467c
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2020-09-03 21:02:48 +0100

    updates

--- examples/produce_examples.cpp
+++ examples/produce_examples.cpp
@@ -11,7 +11,7 @@ void write_random_clauses(std::ostream &out, int num_clauses, int num_vars)
 {
     out << "p cnf " << num_vars << ' ' <<  num_clauses << '\n';
     for (int i(1); i <= num_clauses; ++i) {
-        int rand_num_of_lits (rand() % num_vars + 1); // random number of literals
+        int rand_num_of_lits ((rand() % (num_vars - 1)) + 1); // random number of literals
         for (int j(1); j <= rand_num_of_lits; ++j) {
             // generate random literal
             int rand_sign (rand() % 2);
@@ -38,36 +38,23 @@ int main()
         // create 3 random clauses from a set of 5 variables
         write_random_clauses(of, 3, 5);
         of.close();
-    }/*
-    int example_top_size = std::stoi(argv[1]);
-    for(int i = 2; i <= example_top_size; ++i){
-        // produce file with objective function x_1 + x_2 + ... + x_i
-
-        // create many files with each possible assignment to x_1,...,x_i. There are pow(2,i) assignments.
-        for(int j = 0; j < pow(2,i); ++j){
-            file_name = "hard_";
-            file_name += std::to_string(i) + "_" + std::to_string(j);
+    }
+    for (int i(1); i < 4; ++i) {
+        std::string base_name ("f_");
+        base_name += std::to_string(i);
+        for (int j(1); j < 6; ++j) {
+            std::string file_name (base_name);
+            file_name += '_' + std::to_string(j);
             file_name += ".cnf";
-            std::ofstream constraints_file (file_name, std::ofstream::out);
-            if(!constraints_file){
+            std::ofstream of (file_name, std::ofstream::out);
+            if(!of){
                 std::cerr << "Could not open " << file_name << " for writing" << std::endl;
                 return 1;
             }
-            constraints_file << "p cnf " << std::to_string(i) << " " <<  std::to_string(i) << '\n';
-            int k = j;
-            for(int x = 1; x <= i; ++x){
-                if(k != 0){
-                    if(k % 2 == 0)
-                        constraints_file << std::to_string(-x) << " 0\n";
-                    else
-                        constraints_file << std::to_string(x) << " 0\n";
-                    k = k/2;
-                }
-                else
-                    constraints_file << std::to_string(-x) << " 0\n";
-            }
-            constraints_file.close();
-        }        
-    }*/
+            // create 3 random clauses from a set of 5 variables
+            write_random_clauses(of, 3, 5);
+            of.close();
+        }
+    }
     return 0;
 }
