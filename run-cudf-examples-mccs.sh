9227dcbedaa28c95d9d5515a3355e5995ff75946
Miguel Cabral <miguel.cabral@tecnico.ulisboa.pt>
2020-10-01 10:38:28 +0100

    updates

--- run-cudf-examples-mccs.sh
+++ run-cudf-examples-mccs.sh
@@ -2,8 +2,8 @@
 for f in cudf-examples/*.cudf
 do
     # run mccs on f, get the # lines and put them in m_f.out in /results directory
-    echo "################### MCCS ######################" > "m_$(basename "$f")_tmp.out"
-    ./mccs-1.1/mccs -v1 -i "$f" -leximax[-removed,-notuptodate,-nunsat[recommends:,true],-new] >> "m_$(basename "$f")_tmp.out" 2>> "m_$(basename "$f")_tmp.out"
-    grep '#' "m_$(basename "$f")_tmp.out" > "results/m_$(basename "$f").out"
-    rm "m_$(basename "$f")_tmp.out"
+    echo "################### MCCS ######################" > "results/m_$(basename "$f")_tmp.out"
+    ./mccs-1.1/mccs -v1 -i "$f" -leximax[-removed,-notuptodate,-nunsat[recommends:,true],-new] >> "results/m_$(basename "$f")_tmp.out" 2>> "results/m_$(basename "$f")_tmp.out"
+    grep '#' "results/m_$(basename "$f")_tmp.out" > "results/m_$(basename "$f").out"
+    rm "results/m_$(basename "$f")_tmp.out"
 done
