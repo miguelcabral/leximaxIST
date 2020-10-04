583c406833771a47f909eb1fe4807d0dcd71bfb3
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2020-10-04 14:23:38 +0100

    run scripts updates

--- run-cudf-examples-mccs.sh
+++ run-cudf-examples-mccs.sh
@@ -5,8 +5,7 @@ do
     RESFILE="results/m_$(basename "$f").out"
     # run mccs on f, get the # lines and put them in m_f.out in /results directory
     echo "################### MCCS ######################" > $TMPFILE
-    timeout 20m ./mccs-1.1/mccs -v1 -i "$f" \
+    timeout 20m -k 1s ./mccs-1.1/mccs -v1 -i "$f" \
 -leximax[-removed,-notuptodate,-nunsat[recommends:,true],-new] >> $TMPFILE 2>> $TMPFILE
     grep '#' $TMPFILE > $RESFILE
-    rm $TMPFILE
 done
--- run-packup.sh
+++ run-packup.sh
@@ -4,7 +4,7 @@ TMPFILE=results/p_$(basename $f)_tmp.out
 RESFILE=results/p_$(basename $f).out
 echo "################### PACKUP ####################" > $TMPFILE
 # run packup on f, get the # lines and put them on p_f.out in /results directory
-timeout 20m ./old_packup/packup -t --max-sat --leximax --external-solver \
+timeout 20m -k 1s ./old_packup/packup -t --max-sat --leximax --external-solver \
 "$PATHRC2" $f >> $TMPFILE 2>> $TMPFILE
 grep '#' $TMPFILE > $RESFILE
 rm $TMPFILE
