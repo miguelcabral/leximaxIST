13031e88f2581c1a47a29ecab06c16fa030d0ca3
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2020-10-17 22:55:25 +0100

    updates

--- run-packup.sh
+++ run-packup.sh
@@ -14,18 +14,16 @@ INSTANCE=$6
 SOLUTION=/home/mcabral/data/tmp/$BENCHMARK/p_$SOLVERNAME_$(basename $INSTANCE).sol
 if ! [ -f "$SOLUTION" ]; then
     touch $SOLUTION
-    # INFO contains the time measured by packup and the objective vector
-    INFO=/home/mcabral/data/tmp/$BENCHMARK/p_$SOLVERNAME_$(basename $INSTANCE).info
     # STATS contains the measurements taken by runsolver during the execution
     STATS=/home/mcabral/data/tmp/$BENCHMARK/p_$SOLVERNAME_$(basename $INSTANCE).stats
     # run packup
     if [ "$MAXSAT" = "max-sat" ]; then
         /home/mcabral/runsolver/src/runsolver --vsize-limit 4000 -C 1200 -d 10 -w $STATS \
-(/home/mcabral/thesis/old_packup/packup -u "$CRITERION" --max-sat --leximax \
---external-solver "$SOLVERCMD" $INSTANCE $SOLUTION 2> $INFO)
+-o $SOLUTION /home/mcabral/thesis/old_packup/packup -u "$CRITERION" --max-sat --leximax \
+--external-solver "$SOLVERCMD" $INSTANCE
     else
         /home/mcabral/runsolver/src/runsolver --vsize-limit 4000 -C 1200 -d 10 -w $STATS \
-(/home/mcabral/thesis/old_packup/packup -u "$CRITERION" --leximax \
---external-solver "$SOLVERCMD" $INSTANCE $SOLUTION 2> $INFO)
+-o $SOLUTION /home/mcabral/thesis/old_packup/packup -u "$CRITERION" --leximax \
+--external-solver "$SOLVERCMD" $INSTANCE
     fi
 fi
