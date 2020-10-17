4213c39293a55c0240e0e238d02d36b17cac34a8
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2020-10-17 19:30:17 +0100

    updates - scripts

--- run-all-packup.sh
+++ run-all-packup.sh
@@ -3,7 +3,7 @@ echo "Running packup with the following parameters:\n\\
 \tuser criteria: $2\n\\
 \texternal solver: $4\n\\
 \tmax sat/pbo: $5\n" >> /home/mcabral/what-am-i-running.txt
-find . -type f -exec sh run-packup.sh $1 $2 $3 $4 $5 '{}' \;
+find home/mcabral/data/benchmarks/mancoosi/$1 -type f -exec sh run-packup.sh $1 $2 $3 $4 $5 '{}' \;
 echo "Finnished running packup with the following parameters:\n\\
 \tbenchmarks: $1\n\\
 \tuser criteria: $2\n\\
