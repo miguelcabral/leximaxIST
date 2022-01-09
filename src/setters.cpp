7f377756f73a2c28a8964c796199b6183bdcfdc7
miguelcabral <miguel.cabral@tecnico.ulisboa.pt>
2022-01-09 00:41:42 +0000

    small updates

--- src/setters.cpp
+++ src/setters.cpp
@@ -76,8 +76,7 @@ namespace leximaxIST {
     {
         if (mode != "external" && mode != "bin" && mode != "linear-su" &&
             mode != "linear-us" && mode != "core-static" && mode != "core-dynamic"
-            && mode != "core-dynamic-rebuild" && mode != "core-dynamic-rebuild-incr" 
-            && mode != "core-dynamic-dup") {
+            && mode != "core-dynamic-rebuild" && mode != "core-dynamic-rebuild-incr") {
             print_error_msg("Invalid optimisation mode: '" + mode + "'");
             exit(EXIT_FAILURE);
         }
