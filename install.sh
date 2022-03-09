f13584b2e02ffbfb4a1c660928e6f44ae97a5307
Miguel Bettencourt Moniz Cabral <37836517+miguelcabral@users.noreply.github.com>
2022-03-09 13:57:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,20 @@
-# Install CaDiCaL
-echo "Setting up CaDiCaL..."
-git clone https://github.com/arminbiere/cadical.git
-cd cadical && ./configure --competition && make
-# Compile leximaxIST library
-echo "Compiling leximaxIST library..."
-cd ..
-mkdir -v lib
-cd src && make clean release
-# Compile packup
-echo "Compiling packup..."
-cd ../old_packup/ && make clean release
-cd ..
-echo "Compiling mccs with ILP solver Cbc..."
-echo "Installing Cbc..."
-mkdir -v cbc
-cd cbc
-wget https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
-chmod u+x coinbrew
-./coinbrew fetch Cbc@2.10.7
-./coinbrew build Cbc
-echo "Compiling mccs..."
-mkdir -v ../mccs-1.1/objs
-cd ../mccs-1.1 && make clean mccs-static
+echo "Setting up CaDiCaL..." && \
+git clone https://github.com/arminbiere/cadical.git && \
+cd cadical && ./configure --competition && make && \
+echo "Compiling leximaxIST library..." && \
+cd .. && \
+mkdir -v lib && \
+cd src && make clean release && \
+echo "Compiling packup..." && \
+cd ../old_packup/ && make clean release  && \
+echo "Compiling mccs with ILP solver Cbc..." && \
+echo "Installing Cbc..." && \
+cd .. && mkdir -v cbc && cd cbc && \
+wget https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew && \
+chmod u+x coinbrew && \
+./coinbrew fetch Cbc@2.10.7 && \
+./coinbrew build Cbc && \
+echo "Compiling mccs..." && \
+mkdir -v ../mccs-1.1/objs && \
+cd ../mccs-1.1 && make clean mccs-static && \
 echo "All Done"
